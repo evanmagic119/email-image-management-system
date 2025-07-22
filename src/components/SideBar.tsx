35d848417d95316c09403aa75a284aea41cb667a
evanmagic119 <evan_magic@foxmail.com>
2025-07-21 22:47:18 -0300

    update

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -96,6 +96,7 @@ export default function SideBar() {
             <ListItemButton
               onClick={() => {
                 signOut()
+                localStorage.clear()
                 setDrawerOpen(false)
               }}
             >
