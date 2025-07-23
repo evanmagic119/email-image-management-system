a218c216d777602a4a86e42a46eb64affe311051
evanmagic119 <evan_magic@foxmail.com>
2025-07-23 12:03:44 -0300

    update

--- src/lib/imapUtils.ts
+++ src/lib/imapUtils.ts
@@ -3,18 +3,18 @@ import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
-
 export const getTimeRange = (hoursAgo: number = 1) => {
   const now = new Date()
   const start = new Date(now.getTime() - hoursAgo * 60 * 60 * 1000)
   return { start, end: now }
 }
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 export const formatEmailTime = (date: Date, timezone: string = 'UTC') => {
   return {
     timeUTC: dayjs(date).utc().format('MMMM D, YYYY [at] hh:mm A [UTC]'),
-    timeLocal: dayjs(date).tz(timezone).format('MMMM D, YYYY [at] hh:mm A [Z]')
+    timeLocal: dayjs.tz(date, timezone).format('MMMM D, YYYY [at] hh:mm A [z]')
   }
 }
